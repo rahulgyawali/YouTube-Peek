0b12fec727a8d70888281ef00f27d52abc163acf
Dewan <iit2015097@iiita.ac.in>
2017-01-30 03:55:59 +0530

    Basic structure ready and working

--- myscript.js
+++ myscript.js
@@ -16,12 +16,12 @@ function rqpcr() {
 	//console.log(str);
 	var re = /storyboard_spec\":\"([^"]*)"/;
 	res = str.match(re)[1];
-	// console.log(res);
+	console.log(res);
 	t1 = res.indexOf('|');
 	t2 = res.indexOf('|', t1 + 1);
 	t3 = res.indexOf('|', t2 + 1);
 	fine_res = res.slice(t2 + 1, t3);
-	// console.log(fine_res);
+	console.log(fine_res);
 	obja = {};
 	obja.w = 160;
 	temp = fine_res.indexOf('#');
@@ -52,12 +52,17 @@ function init(v_id) {
 		//console.log(str);
 		var re = /storyboard_spec\":\"([^"]*)"/;
 		res = str.match(re)[1];
-		// console.log(res);
+		console.log(res);
 		t1 = res.indexOf('|');
 		t2 = res.indexOf('|', t1 + 1);
 		t3 = res.indexOf('|', t2 + 1);
-		fine_res = res.slice(t2 + 1, t3);
-		// console.log(fine_res);
+		t4 = res.indexOf('|', t3 + 1);
+		console.log(t4);
+		if(t4 == -1)
+			fine_res = res.slice(t3 + 1);
+		else
+			fine_res = res.slice(t3 + 1, t4);	
+		console.log(fine_res);
 		obja = {};
 		obja.w = 160;
 		temp = fine_res.indexOf('#');
@@ -85,11 +90,53 @@ function send_req() {
 	init(id);
 	tst_elem = document.createElement('div');
 	tst_elem.id = 'slide_board';
-	tst.style.height = 100%;
-	tst.style.width = 100%;
-	this.parentNode.childNodes.add(tst_elem);
+	tst_elem.style.height = '100%';
+	tst_elem.style.width = '100%';
+	tst_elem.style.overflow = 'hidden';
+	tst_elem.style.top = '0px';
+	tst_elem.style.position = 'absolute';
+	tst_elem.style.background = 'white';
+	tst_elem.style.padding = '3px';
+
+
+	this.parentNode.appendChild(tst_elem);
 }
 
-function start_anim() {
-	
+function start_anim(obja) {
+	i = 0;
+	sld_brd = document.getElementById('slide_board');
+	console.log(sld_brd);
+	if (sld_brd.childNodes.length == 0) {
+		console.log(obja);
+		tst_elem1 = document.createElement('div');
+		tst_elem1.style.width = (obja.w).toString() + 'px';
+		tst_elem1.style.height = (obja.h).toString() + 'px';
+		tst_elem = document.createElement('img');
+		tst_elem.id = 'slide_board_image';
+		tst_elem.style.zIndex = '200';
+		tst_elem.style.position = 'absolute'
+		tst_elem.style.top = '0px';
+		tst_elem.style.left = '0px';
+		urll = 'https://i9.ytimg.com/sb/' + obja.v_id + '/storyboard3_L2/M' + i +'.jpg?sigh=' + obja.sigh + '0';
+		tst_elem.src = urll;
+		console.log(urll);
+		tst_elem1.appendChild(tst_elem);
+		sld_brd.appendChild(tst_elem1);
+		mov_start(obja, 0);
+	}
 }
+
+function mov_start(obja, i) {
+	console.log(obja, i);
+	tst_elem = document.getElementById('slide_board_image');
+	w = obja.w;
+	h = obja.h;
+	s_h = obja.strip_h;
+	s_w = obja.strip_w;
+	no = obja.no;
+	tst_elem.style.left = '-' + ((i % s_w) * w).toString() + 'px';
+	tst_elem.style.top = '-' + (Math.floor(i / s_w) * h).toString() + 'px';
+	if(i == no - 1)
+		i = 0;
+	setTimeout(function(){ mov_start(obja, i + 1);}, 1000);
+}	
